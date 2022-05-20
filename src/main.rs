482600200a61b94e384f7285bcb58f7cda244f63
NickLarsenNZ <nick@aptiv.co.nz>
2022-05-20 15:59:21 +0200

    feat: read from k8s api

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-use std::thread;
+use std::error::Error;
+use std::{thread};
 use std::time::Duration;
 
+use k8s_openapi::api::core::v1::Node;
+use kube::{Client, Api, api::ListParams};
 
-fn main() {
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    let kubernetes_client = Client::try_default().await.expect("can't make a k8s client");
+    let nodes: Api<Node> = Api::all(kubernetes_client);
+
+    match nodes.list(&ListParams::default().limit(1)).await {
+        Ok(nodes) => {
+            if let Some(node)= nodes.items.get(0) {
+                if let Some(name) = node.metadata.name.clone() {
+                    println!("node: {}", name);
+                }
+            }
+        },
+        Err(e) => println!("{}", e)
+    }
+
     println!("Sleeping...");
     loop {
-        thread::sleep(Duration::from_secs(1));
+        thread::sleep(Duration::from_secs(15));
     }
 
 }
