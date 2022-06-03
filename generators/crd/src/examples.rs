3632f4ee2985781cd0ec269dec45e1c9956667ba
NickLarsenNZ <nick@aptiv.co.nz>
2022-06-03 23:44:52 +0200

    feat(crd): rework examples, add kustomization for quick test environment setup

--- generators/crd/src/examples.rs
+++ generators/crd/src/examples.rs
@@ -11,23 +11,33 @@ impl SecretMap {
     pub fn examples() -> Vec<Self> {
         vec![
             Self::new("static-example", SecretMapSpec {
-                secret_name: String::from("example"),
+                secret_name: String::from("static-example"),
                 labels: Some(BTreeMap::from([
-                    (String::from("kube-secrets-map.example.com/overwrite"), String::from("true")),
+                    (String::from("app.kubernetes.io/component"), String::from("database")),
                 ])),
                 secret_already_exists: None,
                 mappings: BTreeMap::from([
                     (String::from("DB_USERNAME"), MappingConfig::Static(String::from("postgres"))),
                 ]),
             }),
-            Self::new("aws-example-overwrite", SecretMapSpec {
-                secret_name: "example".to_owned(),
+            Self::new("static-example-overwrite", SecretMapSpec {
+                secret_name: String::from("existing"),
                 labels: None,
                 secret_already_exists: Some(SecretAlreadyExists{
                     match_labels: BTreeMap::from([
                         (String::from("kube-secrets-map.example.com/overwrite"), String::from("true")),
                     ]),
                 }),
+                mappings: BTreeMap::from([
+                    (String::from("DB_USERNAME"), MappingConfig::Static(String::from("postgres"))),
+                ]),
+            }),
+            Self::new("aws-example", SecretMapSpec {
+                secret_name: "aws-example".to_owned(),
+                labels: Some(BTreeMap::from([
+                    (String::from("app.kubernetes.io/component"), String::from("database")),
+                ])),
+                secret_already_exists: None,
                 mappings: BTreeMap::from([
                     (String::from("DB_PASSWORD"), MappingConfig::Aws(AwsProviderConfig {
                         region:Some(String::from("ap-southeast-2")),
